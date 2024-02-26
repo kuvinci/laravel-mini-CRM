4ec264d0abe306e07a36a04f3d170ff0ce94093e
kuvinci <kuvinci@gmail.com>
2024-02-27 00:15:52 +0200

    Adding UserSeeder.php for ease of checking project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CompanySeeder::class,
             EmployeeSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
